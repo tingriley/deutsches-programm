3e9e005e6beead789f2cd7cbab9390d6e0bb5f4a
tingriley <handsomepot@gmail.com>
2021-03-30 09:44:40 +0800

    non recursive

--- suche.c
+++ suche.c
@@ -18,10 +18,32 @@ int binaresuche(int *arr, int links, int rechts, int num)
     return -1;
 }
 
+
+int binaresuche2(int *arr, int links, int rechts, int num)
+{
+    while(links <= rechts){
+        int mid = (links + rechts)/2;
+        if(arr[mid] == num){
+            return mid;     
+        }
+        else if(arr[mid] < num){
+            links = mid + 1;
+        }
+        else{
+            rechts = mid - 1;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
+    int x = -1;
     for(int i = 0; i <= 5; i++){
-        int x = binaresuche(arr, 0, 5, i);
+        x = binaresuche(arr, 0, 5, i);
+        printf("x = %d\n", x);
+
+        x = binaresuche2(arr, 0, 5, i);
         printf("x = %d\n", x);
     }
     return 0;
